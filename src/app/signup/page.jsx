813dd7372a2805ffee55e3ab5b40aa1d108a6294
Rushikesh Patel <ramanirushi123@gmail.com>
2024-10-22 16:52:34 +0530

    Remove invalid export from signup page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -15,8 +15,6 @@ import { Label } from "../../components/ui/label";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
-export const description =
-  "A sign up form with first name, last name, email and password inside a card. There's an option to sign up with GitHub and a link to login if you already have an account";
 
 export default function SignUpForm() {
   const {
